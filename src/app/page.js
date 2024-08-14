5ae7a8e0351392d831424ca1fb750a03cc0f9a5d
DePINC Dev <dev@depinc.org>
2024-08-14 23:31:23 +0800

    Restore Rank(s).

--- src/app/page.js
+++ src/app/page.js
@@ -205,6 +205,30 @@ function Title({ server_ip }) {
     );
 }
 
+function Rank({ rank, title }) {
+    let begin_height;
+    let entries = [];
+    if (rank) {
+        begin_height = rank.begin_height;
+        entries = rank.entries;
+    }
+    return (
+        <>
+            <SectionTitle Icon={FaHackerrank} title={title} desc={'Since height ' + formatNumberString(begin_height)} />
+            {entries.map((entry, i) => {
+                return (
+                    <div className="mb-2">
+                        <StatusEntry name="Farmer public-key" strong_value value={entry.farmer_pk} hi={i % 2 === 0} />
+                        <StatusEntry name="Produced blocks" value={formatNumberString(entry.count)} hi={i % 2 === 0} />
+                        <StatusEntry name="Avg. Block Difficulty" value={formatNumberString(entry.average_difficulty)} hi={i % 2 === 0} />
+                        <StatusEntry name="Rewards" value={formatNumberString(entry.total_reward / 100000000) + ' DEPC'} hi={i % 2 === 0} />
+                    </div>
+                );
+            })}
+        </>
+    );
+}
+
 /**
  * Base status
  */
@@ -600,12 +624,16 @@ export default function Home() {
     const [summary24_7, querySummary24_7] = useTimelordSummary(24 * 7);
     const [netspace, queryNetspace] = useTimelordNetspace(24 * 7);
     const [fullMortgageInfo, queryFullMortgageInfo] = useAccumulatedBlocks();
+    const [rank, queryRank] = useRank();
+    const [rank24, queryRank24] = useRank(24);
     const [pageNo, setPageNo] = useState(0);
     useEffect(() => {
         queryStatus();
         querySummary24();
         querySummary24_7();
         queryNetspace();
+        queryRank();
+        queryRank24();
         queryFullMortgageInfo(pageNo * ENTRIES_PER_PAGE, ENTRIES_PER_PAGE);
     }, [pageNo]);
     return (
@@ -622,6 +650,14 @@ export default function Home() {
                     <div className="lg:flex lg:flex-row lg:justify-between lg:p-8">
                         <Summary {...summary24} />
                         <Summary {...summary24_7} />
+                    </div>
+                    <div className="lg:bg-gray-50 dark:lg:bg-gray-700 lg:rounded-2xl">
+                        <div className="mt-4 lg:p-8">
+                            <Rank title="Rank" rank={rank} />
+                        </div>
+                        <div className="my-4 lg:p-8">
+                            <Rank title="Rank in 24 hours" rank={rank24} />
+                        </div>
                     </div>
                     <div className="lg:bg-gray-50 dark:lg:bg-gray-700 lg:rounded-2xl">
                         <div className="mt-4 lg:p-8">
